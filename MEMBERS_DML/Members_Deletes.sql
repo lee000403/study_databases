9b1af097eb42c3c9fc9d6a211b800c0da627a9a8
lee000403 <dong000403@gmail.com>
2023-06-07 12:46:03 +0900

    COMMIT

--- MEMBERS_DML/Members_Deletes.sql
+++ MEMBERS_DML/Members_Deletes.sql
@@ -1,23 +1,6 @@
--- 취미 (PK삭제)
-DELETE FROM members_hobby
-where MEMBERS_ID LIKE 'M%';
-
--- 로그인(PK삭제)
-DELETE FROM membewrs_login
-WHERE MEMBERS_ID LIKE 'M%';
-
--- 취미 코드 삭제
-DELETE FROM hobby_code
-WHERE HOBBY_CODE_ID LIKE 'H%';
-
--- 성별 코드 삭제
-DELETE FROM gender_code
-WHERE GENDER_CODE_ID LIKE 'G%';
-
--- 주소 코드 삭제
-DELETE FROM address_code
-WHERE ADDRESS_CODE_ID LIKE 'A%';
-
--- 회원정보 삭제
-DELETE FROM members
-WHERE TELEPHONE LIKE 'M%';
+DELETE FROM members_hobby ;
+DELETE FROM membews_login ;
+DELETE FROM hobby_code ;
+DELETE FROM members ;
+DELETE FROM address_code ;
+DELETE FROM gender_code ;
