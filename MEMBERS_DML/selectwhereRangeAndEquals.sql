535e0495a8d3b7899271891c6b5c264cb813a658
lee000403 <dong000403@gmail.com>
2023-06-07 15:42:36 +0900

    commit

---
+++ MEMBERS_DML/selectwhereRangeAndEquals.sql
@@ -0,0 +1,4 @@
+SELECT * FROM Customers
+WHERE (CustomerID BETWEEN 50 AND 89) AND (City='London');
+-- Number of Records: 2
+-- refer:https://www.w3schools.com/mysql/trymysql.asp?filename=trysql_select_all
