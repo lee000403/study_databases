3b92f723867f62d5a59f534625eaeaf62af36bc1
lee000403 <dong000403@gmail.com>
2023-06-08 16:12:04 +0900

    COMMIIT

--- MEMBERS_DML/OrdersMaxEmployee.sql
+++ MEMBERS_DML/OrdersMaxEmployee.sql
@@ -1,10 +1,10 @@
 SELECT MAX(CNT_ALL.CNT) AS MAX_CNT, CNT_ALL.NAME -- 서브 쿼리 이용으로 MAX 값 찾기
 FROM ( -- 최대값을 얻기 위한 COUNT 사용
-SELECT ORD.EmployeeID, COUNT(ORD.EmployeeID) AS CNT, EMP.LastName AS NAME 
-FROM Orders AS ORD
-JOIN Employees AS EMP
-ON ORD.EmployeeID = EMP.EmployeeID
-GROUP BY ORD.EmployeeID
+	SELECT ORD.EmployeeID, COUNT(ORD.EmployeeID) AS CNT, EMP.LastName AS NAME 
+	FROM Orders AS ORD
+	JOIN Employees AS EMP
+	ON ORD.EmployeeID = EMP.EmployeeID
+	GROUP BY ORD.EmployeeID
 ) AS CNT_ALL
 ;
 
