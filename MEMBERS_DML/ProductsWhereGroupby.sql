58f6219d92b8c4a051d56240b7ff05d0bb302179
lee000403 <dong000403@gmail.com>
2023-06-08 11:03:50 +0900

    COM

--- MEMBERS_DML/ProductsWhereGroupby.sql
+++ MEMBERS_DML/ProductsWhereGroupby.sql
@@ -1,6 +1,6 @@
 SELECT T_PR.CategoryID, COUNT(T_PR.CategoryID) AS CNT_CategoryID  -- AS 를 통해 총 개수 COLUMN 이름 변환
 FROM Products AS T_PR
-GROUP BY CategoryID
+GROUP BY T_PR.CategoryID
 HAVING COUNT(T_PR.CategoryID) >= 10; -- HAVING 으로 GROUP 조건 지정
 
 -- Number of Records: 5
